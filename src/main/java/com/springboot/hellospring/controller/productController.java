a9bad7584b5bacb2fc561aa7f591bc7bc9fe87f5
ayush141997 <ayushgupta555544@gmail.com>
2019-02-12 15:26:37 +0530

    query

--- src/main/java/com/springboot/hellospring/controller/productController.java
+++ src/main/java/com/springboot/hellospring/controller/productController.java
@@ -8,6 +8,8 @@ import com.springboot.hellospring.dao.product;
 import com.springboot.hellospring.model.productModel;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +26,18 @@ public class productController{
 
     @RequestMapping(value="/product", method=RequestMethod.POST)
     public @ResponseBody String createProduct(@RequestBody product prods,HttpServletResponse res) {
+        try {
             productrepo.save(prods);
             return "Created";
+        } catch(TransactionSystemException t){
+            System.err.println(t);
+            res.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+            return "Either of the inputs are null";
+        } catch(DataIntegrityViolationException d){
+            System.err.println(d);
+            res.setStatus(HttpServletResponse.SC_CONFLICT);
+            return "Constraint Violation";
+        }
     }
     
     @RequestMapping(value="/product/{ch}", method=RequestMethod.GET)
--- src/main/java/com/springboot/hellospring/model/productModel.java
+++ src/main/java/com/springboot/hellospring/model/productModel.java
@@ -1,10 +1,12 @@
 package com.springboot.hellospring.model;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import com.springboot.hellospring.dao.*;
 
 public interface productModel extends CrudRepository<product, Long> {
     
-    List<product> findByPnameContaining(String pname);
+    @Query(value = "Select * from product where product.pname like %?1%", nativeQuery = true)
+    public List<product> findByPnameContaining(String pname);
 }
