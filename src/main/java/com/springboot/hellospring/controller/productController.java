3d024b1159d123e8ef78afaf86e83799b7cc2a25
ayush141997 <ayushgupta555544@gmail.com>
2019-02-12 15:54:50 +0530

    exception commit

--- src/main/java/com/springboot/hellospring/controller/productController.java
+++ src/main/java/com/springboot/hellospring/controller/productController.java
@@ -8,8 +8,6 @@ import com.springboot.hellospring.dao.product;
 import com.springboot.hellospring.model.productModel;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
